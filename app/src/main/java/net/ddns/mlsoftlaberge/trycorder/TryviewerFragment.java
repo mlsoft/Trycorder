1eb31093f93fd32c307eb3261c3e5b714f08d4a2
Martin Laberge <mlsoftlaberge@gmail.com>
2016-06-22 15:05:57 -0400

    added a video viewer and a list in the tryviewer fragment

--- app/src/main/java/net/ddns/mlsoftlaberge/trycorder/TryviewerFragment.java
+++ app/src/main/java/net/ddns/mlsoftlaberge/trycorder/TryviewerFragment.java
@@ -4,16 +4,26 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mlsoft on 16-06-20.
@@ -52,10 +62,10 @@ public class TryviewerFragment extends Fragment {
     private String displayLanguage;
 
     // the button to talk to computer
-    private ImageButton mTalkButton;
+    private ImageButton mBacktopButton;
 
     // the button for sound activation
-    private Button mSoundButton;
+    private Button mBackButton;
 
     // the button to start it all
     private Button mStartButton;
@@ -65,7 +75,7 @@ public class TryviewerFragment extends Fragment {
     private boolean mRunStatus = false;
 
     // the button for settings
-    private Button mModeButton;
+    private Button mResetButton;
 
     // the one status line
     private TextView mTextstatus_top;
@@ -89,6 +99,12 @@ public class TryviewerFragment extends Fragment {
     // the main contents layout in the center
     private LinearLayout mCenterLayout;
 
+    // the list of videos to view
+    private ListView mListView;
+
+    // the video view widget to show contents
+    private VideoView mVideoView;
+
     // the preferences holder
     private SharedPreferences sharedPref;
 
@@ -108,11 +124,22 @@ public class TryviewerFragment extends Fragment {
 
         // ===================== top horizontal button grid ==========================
         // the start button
-        mTalkButton = (ImageButton) view.findViewById(R.id.talk_button);
-        mTalkButton.setOnClickListener(new View.OnClickListener() {
+        mBacktopButton = (ImageButton) view.findViewById(R.id.backtop_button);
+        mBacktopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                buttonbad();
+                buttonsound();
+                switchtryviewermode(1);
+            }
+        });
+
+        // the sound-effect button
+        mBackButton = (Button) view.findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonsound();
+                switchtryviewermode(1);
             }
         });
 
@@ -122,6 +149,7 @@ public class TryviewerFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 buttonsound();
+                startvideo();
             }
         });
 
@@ -131,30 +159,21 @@ public class TryviewerFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 buttonsound();
-            }
-        });
-
-        // the sound-effect button
-        mSoundButton = (Button) view.findViewById(R.id.sound_button);
-        mSoundButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                buttonsound();
+                stopvideo();
             }
         });
 
         // the settings button
-        mModeButton = (Button) view.findViewById(R.id.mode_button);
-        mModeButton.setOnClickListener(new View.OnClickListener() {
+        mResetButton = (Button) view.findViewById(R.id.reset_button);
+        mResetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 buttonsound();
-                switchtryviewermode(1);
+                resetvideo();
             }
         });
 
         mTextstatus_top = (TextView) view.findViewById(R.id.textstatus_top);
-        mTextstatus_top.setText("");
 
         // ===================== bottom horizontal button grid ==========================
         // the ask button
@@ -205,13 +224,20 @@ public class TryviewerFragment extends Fragment {
         mTextstatus_bottom = (TextView) view.findViewById(R.id.textstatus_bottom);
         mTextstatus_bottom.setText("Ready");
 
+        // =========================== center content area ============================
+
         // the center layout to show contents
         mCenterLayout = (LinearLayout) view.findViewById(R.id.center_layout);
+
+        mListView = (ListView) view.findViewById(R.id.list_view);
+
+        mVideoView = (VideoView) view.findViewById(R.id.video_view);
 
         return view;
 
     }
 
+    // setup the fonts on every text-containing widgets
     @Override
     public void onActivityCreated(Bundle bundle) {
         super.onActivityCreated(bundle);
@@ -221,8 +247,8 @@ public class TryviewerFragment extends Fragment {
         // top buttons
         mStartButton.setTypeface(face2);
         mStopButton.setTypeface(face2);
-        mSoundButton.setTypeface(face2);
-        mModeButton.setTypeface(face2);
+        mBackButton.setTypeface(face2);
+        mResetButton.setTypeface(face2);
         mTextstatus_top.setTypeface(face);
         // bottom buttons
         mSnapButton.setTypeface(face2);
@@ -230,6 +256,8 @@ public class TryviewerFragment extends Fragment {
         mRecordButton.setTypeface(face2);
         mGalleryButton.setTypeface(face2);
         mTextstatus_bottom.setTypeface(face3);
+        // start the filling of the list
+        filllistview();
     }
 
     @Override
@@ -257,21 +285,105 @@ public class TryviewerFragment extends Fragment {
         super.onPause();
     }
 
+    // ask the activity to switch to another fragment
     private void switchtryviewermode(int mode) {
         mOnTryviewerInteractionListener.onTryviewerModeChange(mode);
     }
 
-
+    // beep
     private void buttonsound() {
         MediaPlayer mediaPlayer = MediaPlayer.create(getActivity().getBaseContext(), R.raw.keyok2);
         mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
 
+    // booooop
     private void buttonbad() {
         MediaPlayer mediaPlayer = MediaPlayer.create(getActivity().getBaseContext(), R.raw.denybeep1);
         mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
 
+    // tell something in the bottom status line
+    private void say(String texte) {
+        mTextstatus_bottom.setText(texte);
+    }
+
+    // =======================================================================================
+
+    //private Uri staticUri =  Uri.parse("android.resource://"
+    //        + getActivity().getPackageName()
+    //        + "/" + R.raw.powers_of_ten);
+    private Uri staticUri =  Uri.parse("android.resource://net.ddns.mlsoftlaberge.trycorder"
+            + "/" + R.raw.powers_of_ten);
+
+    private Uri dynamicUri = null;
+
+    private MediaController mMediaController=null;
+
+    private void startvideo() {
+        say("Start Video");
+        if(mMediaController==null) {
+            mMediaController = new MediaController(getActivity());
+            mMediaController.setAnchorView(mVideoView);
+            mVideoView.setMediaController(mMediaController);
+        }
+        if(dynamicUri==null) {
+            mVideoView.setVideoURI(staticUri);
+        } else {
+            mVideoView.setVideoURI(dynamicUri);
+        }
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+                mVideoView.start();
+                say("Video is started");
+            }
+        });
+        //mVideoView.start();
+    }
+
+    private void stopvideo() {
+        mVideoView.pause();
+        say("Video is paused");
+    }
+
+    private void resetvideo() {
+        mVideoView.start();
+        say("Video is restarted");
+    }
+
+    // ======================== Image list loader ==========================================
+
+    private List<Uri> mImageUris = new ArrayList<Uri>();
+    private int currenturi=0;
+
+    private void filllistview() {
+        loadimageuris();
+        if (mImageUris.size()>0) {
+            currenturi=0;
+            dynamicUri=mImageUris.get(currenturi);
+        }
+    }
+
+    private void loadimageuris() {
+        say("Loading videos Uris");
+        String path = Environment.getExternalStorageDirectory().toString()+"/DCIM/Camera";
+        Log.d("Files", "Path: " + path);
+        File f = new File(path);
+        File file[] = f.listFiles();
+        Log.d("Files", "Size: "+ file.length);
+        for (int i=0; i < file.length; i++)
+        {
+            String name = file[i].getName();
+            if(name.contains(".mp4")) {
+                Log.d("Files", "FileName:" + name);
+                Uri uri = Uri.parse(path + "/" + name);
+                mImageUris.add(uri);
+            }
+        }
+        say("Videos Uris loaded");
+    }
 
 
 }
